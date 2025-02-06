b00cb8668c154f3aab85a15728539c4fa29b16a4
高铭 <101926247+GaomingL1n@users.noreply.github.com>
2025-02-06 16:23:16 +0800

    Update dataset_drug_high.py

--- datasets/dataset_drug_high.py
+++ datasets/dataset_drug_high.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 
-# 1. 读取CSV文件
 df = pd.read_csv('../datasets/bindingdb/random/train_with_id.csv')
 
-# 2. 筛选正样本并去重
+# select pos samples
 positive_samples = df[df['Y'] == 1]
 negative_samples = df[df['Y'] == 1]
 
@@ -14,11 +13,11 @@ unique_positive_samples_protein = unique_positive_samples_smiles.drop_duplicates
 collection_A = unique_positive_samples_protein.reset_index(drop=True)
 
 
-# 3. 取出不在集合A中出现过的SMILES
+# smiles not in A
 all_smiles_in_collection_A = set(collection_A['SMILES'])
 negative_candidates = df[~df['SMILES'].isin(all_smiles_in_collection_A)]
-# 4. 为集合A中的每个蛋白质分配一个新的SMILES作为负样本，构成负样本集合B
-np.random.seed(0)  # 设置随机种子以确保结果可重复
+# assign a new SMILES as a negative sample to each protein in set A, forming negative sample set B
+np.random.seed(0)  
 negative_samples_list = []
 
 for protein in collection_A['Protein']:
@@ -30,7 +29,7 @@ for protein in collection_A['Protein']:
 
 collection_B = pd.DataFrame(negative_samples_list)
 
-# 4. 合并正负样本并保存到新的CSV文件
+# concat
 combined_samples = pd.concat([collection_A, collection_B], ignore_index=True)
 combined_samples.to_csv('../datasets/bindingdb/random/drug_high_train.csv', index=False)
 
