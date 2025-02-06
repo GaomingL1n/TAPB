30ca4c44c3bb4ef983e67325db9e8d39709cced6
高铭 <101926247+GaomingL1n@users.noreply.github.com>
2025-02-06 11:42:48 +0800

    Update dataloader.py

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -107,15 +107,12 @@ def mask_tokens(inputs, attention_mask, tokenizer, probability=0.15):
 
 def drop_tokens(batch, drop_prob=0.75):
     batch_id, batch_mask = batch['input_ids'], batch['attention_mask']
-    num_to_retain = int(max(1, batch_id.size(1) * (1-drop_prob))) # 确保至少保留1个词
+    num_to_retain = int(max(1, batch_id.size(1) * (1-drop_prob))) # at least one token
 
-    # 随机选择要保留的索引，确保每个索引只被选中一次
     indices = sorted(random.sample(range(1, batch_id.size(1)), num_to_retain))
     indices.insert(0, 0)
-    # 根据选中的索引保留相应的词
     batch_id = batch_id[:, indices]
     batch_mask = batch_mask[:, indices]
-    # 将结果转换回Python列表并返回
     return batch_id, batch_mask
 
 def get_dataLoader(batch_size, dataset, drug_tokenizer, shuffle=False, MLM=False):
